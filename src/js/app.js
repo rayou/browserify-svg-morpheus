01c4833efa5574009701627ed7477f001c4a2484
Ray Ou <ray_ou@miii.tv>
2014-12-26 16:30:34 +0800

    SVG Morpheus with browerify-shim

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,15 @@
-var hello = require('./module/hello')
+var SVGMorpheus = require('SVGMorpheus');
+var $ = require('jquery')
 
-hello('world');
+var svg = new SVGMorpheus(document.getElementById('player-control'), {
+  duration: 500,
+  easing: 'cubic-out'
+});
+
+$('#btn-play').click(function(){
+    svg.to('icon-pause');
+})
+
+$('#btn-pause').click(function(){
+    svg.to('icon-play');
+})
